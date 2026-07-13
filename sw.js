33a0359474103c7acfe735310506a5234a649ef7
Anish Bhattarai <bhattaraianish754@gmail.com>
2026-07-13 23:58:02 +0545

    re

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cardfile-v1';
+const CACHE = 'cardfile-v2';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
